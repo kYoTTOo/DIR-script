5187ddefc6bcc5f9381c3fa5b809a966ad74ac6d
kYoTTOo <83626315+kYoTTOo@users.noreply.github.com>
2021-06-04 22:25:52 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 aa=4 ab=5 ac=6
-let aa equal aa + ac
+let aa equal aa add ac
 echo “The first operation is equal to $aa” 
-let ac equal ac - ab
+let ac equal ac minus ab
 echo “The second operation is equal to $ac”
